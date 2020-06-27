2f37581c9d8abf847a17b69e5cc5505bfcf8870b
nikhil mohan <nikmohan81@gmail.com>
2020-06-27 20:10:04 +0530

    Update server.js

--- result/server.js
+++ result/server.js
@@ -52,6 +52,8 @@ function getVotes(client) {
       console.error("Error performing query: " + err);
     } else {
       var votes = collectVotesFromResult(result);
+      console.log("result " + result);
+      console.log("votes " + JSON.stringify(votes));
       io.sockets.emit("scores", JSON.stringify(votes));
     }
 
